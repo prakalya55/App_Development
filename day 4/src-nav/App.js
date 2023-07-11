6bc3be88ccca6ad5afece24e53ec13f0723e54d5
prakalya55 <119046744+prakalya55@users.noreply.github.com>
2023-07-11 16:02:06 +0530

    Add files via upload

---
+++ day 4/src-nav/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles.css";
+
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// PAGES
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Reports from "./pages/Reports";
+import Main from "./login/main";
+import Appp from "./login/app";
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/up" component={Main} />
+          <Route path="/log" component={Appp} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
